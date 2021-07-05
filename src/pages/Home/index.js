7a74c61c8832bdeb569b71973a39bc77c4c9e151
Aramen Meza <arammendoza.97@gmail.com>
2021-07-05 12:37:52 -0500

    Project settings

---
+++ src/pages/Home/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Container, Typography } from "@material-ui/core";
+
+export default () => (
+	<Container><Typography variant="h1">Hellow wolrd!</Typography></Container>
+)
